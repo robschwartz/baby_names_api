a692c0a027f98d6e831baa46a89f9c6a582bae98
Rob Schwartz <rob.schwartz@vibes.com>
2020-05-08 10:05:29 -0400

    update: added validations to babyname

--- app/models/baby_name.rb
+++ app/models/baby_name.rb
@@ -1,3 +1,14 @@
 class BabyName < ApplicationRecord
   belongs_to :list
+  
+  validates_uniqueness_of :name, :scope => :list_id
+
+  before_save :remove_white_space
+
+  private
+
+  def remove_white_space
+    self.name.strip!
+  end
+
 end
